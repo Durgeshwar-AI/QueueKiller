0797b29f1ac7ba3f1cd041b112f6fcf8fc9ddeee
Durgeshwar-AI <durgesh34shaw94@gmail.com>
2025-10-26 03:28:24 +0530

    Tauri initial setup

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,21 +1,23 @@
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
 import morgan from "morgan";
 import helmet from "helmet";
 import { config } from "dotenv";
 import scheduleRoutes from "./routes/schedule.route";
 import rateLimitMiddleware from "./middlewares/rateLimiter";
+import mongoSanitize from "express-mongo-sanitize";
 
 const app = express();
 config();
 
 // Middleware
+app.disable("x-powered-by");
 app.use(helmet());
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(mongoSanitize());
 app.use(morgan("dev"));
-app.use(bodyParser.urlencoded({ extended: true }));
 app.use(rateLimitMiddleware);
 
 app.use("/api/schedule", scheduleRoutes);
