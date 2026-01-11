1f8eca7229dff253d09d64fd92bc4120429c45c6
Durgeshwar-AI <durgesh34shaw94@gmail.com>
2026-01-11 21:24:02 +0530

    Set the min window and start at maximum with initial setup for redux

--- desktopApp/src/App.tsx
+++ desktopApp/src/App.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Login from './pages/Login'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 const App = () => {
+  const isLoggedIn = true;
   return (
     <Routes>
-      <Route path='/' element={<Login/>}/>
+      {!isLoggedIn ? (
+        <Route path="/" element={<Login />} />
+      ) : (
+        <Route path="/" element={<Home />} />
+      )}
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
