52800ff5b09af40d9d59530ef8a47b875c17b3e3
Durgeshwar-AI <durgesh34shaw94@gmail.com>
2026-01-24 19:59:03 +0530

    feat(admin): setup to add an initial admin

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,22 @@
 import { config } from "dotenv";
 import app from "./app";
 import { fillBucket, job } from "./middlewares/rateLimiter";
-import { connectDB } from "./utils/db";
 import { connectRedis } from "./utils/redis";
 import prisma from "./utils/client";
+import { registerAdmin } from "./services/admin.service";
 config();
 const PORT = process.env.PORT || 5000;
 async function startServer() {
   try {
     // 1️⃣ Connect to MongoDB
-    await connectDB();
+    // await connectDB();
     await connectRedis();
     await fillBucket();
     await prisma.$connect();
     console.log("✓ Prisma connected to database");
     job.start();
     console.log("Bucket refill job started");
+    await registerAdmin();
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
---
+++ server/src/services/admin.service.ts
@@ -0,0 +1,18 @@
+import bcrypt from "bcrypt";
+import prisma from "../utils/client";
+
+export const registerAdmin = async () => {
+  const email = process.env.ADMIN_EMAIL;
+  const password = process.env.ADMIN_PASSWORD;
+  const name = "Admin";
+  if (!email || !password) return;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  await prisma.admin.create({
+    data: {
+      name: name,
+      email: email,
+      password: hashedPassword,
+    },
+  });
+  console.log("Initial admin online");
+};
